6637c79491f92575fdd7e71ef1aeb004c262d9a1
Alex Cho <alexsjcho@gmail.com>
2019-02-07 15:00:20 -0800

    tested out jQuery starter functions with console.log

--- scripts.js
+++ scripts.js
@@ -1,1 +1,12 @@
 "use strict";
+
+$(document).ready(function() {
+  console.log("I'm ready baby!");
+  searchUserInput();
+});
+
+function searchUserInput() {
+  $("#search-user").on("keyup", function() {
+    console.log("key pressed");
+  });
+}
