3f17a7bacba04cae69d3cb11cbcf8a4abf6c4c7b
Alex Cho <alexsjcho@gmail.com>
2019-02-07 17:02:04 -0800

    attempting to finish this project using ajax

--- scripts.js
+++ scripts.js
@@ -8,8 +8,6 @@ Client Secret
 2568375eb617ab6ac52a9380ec2c21d7cf91d9c7
 */
 
-const apiKey = "2568375eb617ab6ac52a9380ec2c21d7cf91d9c7";
-
 $(document).ready(function() {
   console.log("Ready to fetch GitHub User Repo!");
   userInput();
@@ -26,7 +24,7 @@ function userInput() {
 
 //Make Request to GitHub API
 function fetchUserName() {
-  fetch(`https://api.github.com/users/${userInput}/repos`)
+  fetch(`https://api.github.com/users/alexsjcho/repos`)
     .then(response => response.json())
     .then(responseJson => displayResults(responseJson))
     .catch(error => alert("Hmmm. Cannot find GitHub UserName"));
@@ -34,20 +32,22 @@ function fetchUserName() {
 
 //Render Repos to the DOM
 
-function displayResults() {
+function displayResults(responseJson) {
   console.log(responseJson);
   $("#display-profile").empty();
-  $("#display-profile").html(`<div class="panel panel-default">
-  <div class="panel-heading">
-    <h3 class="panel-title">${repos.name}</h3>
-  </div>
-  <div class="panel-body">
-   <div class= "row>
-   <div class="col-md-3">
-   <a href=" ${repos.repos_url}">Repo URL Link</a>
-   </div>
-  </div>
-</div>`);
+  responseJson.message.forEach(userRepo => {
+    $("#display-profile").html(`<div class="panel panel-default">
+    <div class="panel-heading">
+      <h3 class="panel-title">${repos.name}</h3>
+    </div>
+    <div class="panel-body">
+     <div class= "row>
+     <div class="col-md-3">
+     <a href=" ${repos.repos_url}">Repo URL Link</a>
+     </div>
+    </div>
+  </div>`);
+  });
   //display the results section
   $(".display-results-container").removeClass("hidden");
 }
